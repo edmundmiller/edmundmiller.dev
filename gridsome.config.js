374f6e0a6a832388fdd4881653dd7c7ef87b878b
Edmund Miller <edmund.a.miller@protonmail.com>
2020-03-05 17:05:00 -0600

    feat(meta): update my personal info

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,25 +5,27 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome Blog Starter',
-  siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.',
+  siteName: "Edmund Miller",
+  siteUrl: "https://edmundmiller.dev",
+  // siteDescription:
+  //   "",
 
   templates: {
-    Post: '/:title',
-    Tag: '/tag/:id'
+    Post: "/:title",
+    Tag: "/tag/:id"
   },
 
   plugins: [
     {
       // Create posts from markdown files
-      use: '@gridsome/source-filesystem',
+      use: "@gridsome/source-filesystem",
       options: {
-        typeName: 'Post',
-        path: 'content/posts/*.md',
+        typeName: "Post",
+        path: "content/posts/*.md",
         refs: {
           // Creates a GraphQL collection from 'tags' in front-matter and adds a reference.
           tags: {
-            typeName: 'Tag',
+            typeName: "Tag",
             create: true
           }
         }
@@ -34,12 +36,10 @@ module.exports = {
   transformers: {
     //Add markdown support to all file-system sources
     remark: {
-      externalLinksTarget: '_blank',
-      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-      anchorClassName: 'icon icon-link',
-      plugins: [
-        '@gridsome/remark-prismjs'
-      ]
+      externalLinksTarget: "_blank",
+      externalLinksRel: ["nofollow", "noopener", "noreferrer"],
+      anchorClassName: "icon icon-link",
+      plugins: ["@gridsome/remark-prismjs"]
     }
   }
-}
+};
--- src/components/Author.vue
+++ src/components/Author.vue
@@ -1,19 +1,18 @@
 <template>
 	<div class="author">
 
-		<g-image alt="Author image" class="author__image" src="~/assets/images/author.jpg" width="180" height="180" blur="5" />
+		<!-- <g-image alt="Author image" class="author__image" src="~/assets/images/author.jpg" width="180" height="180" blur="5" /> -->
 
 		<h1 v-if="showTitle" class="author__site-title">
 			{{ $static.metadata.siteName }}
 		</h1>
 
 		<p class="author__intro">
-			A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.
+			Welcome! I'm a Computational Biologist with a passion for Data.
 		</p>
 
 		<p class="author__links">
-			<a href="//twitter.com/gridsome">Follow on Twitter</a>
-			<a href="//github.com/gridsome/gridsome-starter-blog">GitHub</a>
+			<a href="//github.com/emiller88">GitHub</a>
 		</p>
 
 	</div>
