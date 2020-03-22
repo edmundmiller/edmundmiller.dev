293248d390d3bcbc401ac8e9a74de62e313ddea8
Edmund Miller <edmund.a.miller@protonmail.com>
2020-03-22 14:55:19 -0500

    refactor(tailwind): rewrite default layout

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,25 +1,29 @@
 <template>
   <div id="app">
-    <header class="header">
-      <div class="header__left">
+    <header
+      class="header flex justify-between items-center min-h-50 p-3 top-0 z-10 lg:sticky lg:w-full"
+    >
+      <div class="header__left flex items-center">
         <Logo v-if="showLogo" />
       </div>
 
-      <div class="header__right">
+      <div class="header__right flex items-center">
         <ToggleTheme />
       </div>
     </header>
 
-    <main class="main">
+    <main class="main m-0 m-auto px-15 p-0">
       <slot />
     </main>
 
-    <footer class="footer">
+    <footer
+      class="footer flex items-center justify-center p-2 text-center text-sm"
+    >
       <span class="footer__copyright"
         >Copyright © {{ new Date().getFullYear() }}.
       </span>
-      <span class="footer__links"
-        >Powered by <a href="//gridsome.org"> Gridsome </a></span
+      <span class="footer__links m-1"
+        >Powered by <a class="brand" href="//gridsome.org"> Gridsome </a></span
       >
     </footer>
   </div>
