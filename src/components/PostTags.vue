2311deec0e312c08c84f0df99dd5710b777e124f
Edmund Miller <edmund.a.miller@protonmail.com>
2020-03-21 23:40:49 -0500

    refactor(tailwind): rewrite Post components

--- src/components/PostTags.vue
+++ src/components/PostTags.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="post-tags">
+  <div class="post-tags m-4">
     <g-link
-      class="post-tags__link"
+      class="post-tags__link mr-3 text-sm no-underline bg-brand p-2 rounded"
       v-for="tag in post.tags"
       :key="tag.id"
       :to="tag.path"
