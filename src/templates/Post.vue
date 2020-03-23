9194e57fdf9a332f881d1cef484e488b35dc23a1
Edmund Miller <edmund.a.miller@protonmail.com>
2020-03-22 21:05:54 -0500

    refactor(tailwind): update post
    TODO: still need to do the post and content-box

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -1,34 +1,37 @@
 <template>
   <Layout>
-    <div class="post-title">
+    <div class="post-title py-6 px-0 text-center">
       <h1 class="post-title__text">
         {{ $page.post.title }}
       </h1>
 
       <PostMeta :post="$page.post" />
     </div>
-
+    <!-- TODO -->
     <div class="post content-box">
-      <div class="post__header">
+      <div
+        class="post__header w-24 -ml-10 -mt-10 mb-6 overflow-hidden rounded-t-md"
+      >
         <g-image
+          class="w-full"
           alt="Cover image"
           v-if="$page.post.cover_image"
           :src="$page.post.cover_image"
         />
       </div>
 
-      <div class="post__content" v-html="$page.post.content" />
+      <div class="post__content first:mt-0" v-html="$page.post.content" />
 
       <div class="post__footer">
         <PostTags :post="$page.post" />
       </div>
     </div>
 
-    <div class="post-comments">
+    <div class="post-comments p-6">
       <!-- Add comment widgets here -->
     </div>
 
-    <Author class="post-author" />
+    <Author class="post-author mt-6" />
   </Layout>
 </template>
 
