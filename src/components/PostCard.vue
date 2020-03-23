3007c92d3440547088829453944be66f2a0797c2
Edmund Miller <edmund.a.miller@protonmail.com>
2020-03-22 21:13:07 -0500

    refactor(tailwind): initial PostCard rewrite

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,25 +1,23 @@
 <template>
   <div
-    class="post-card content-box"
-    :class="{ 'post-card--has-poster': post.poster }"
+    class="max-w-4xl px-10 py-6 bg-background rounded-lg shadow-md transition duration-500 transform hover:-translate-y-4 transition duration-500 hover:shadow-xl"
   >
-    <div class="post-card__header">
-      <g-image
-        alt="Cover image"
-        v-if="post.cover_image"
-        class="post-card__image"
-        :src="post.cover_image"
+    <div class="mt-2">
+      <h2
+        class="text-2xl text-gray-700 font-bold hover:text-gray-600"
+        v-html="post.title"
       />
+      <p class="mt-2 text-gray-600" v-html="post.description" />
     </div>
-    <div class="post-card__content">
-      <h2 class="post-card__title" v-html="post.title" />
-      <p class="post-card__description" v-html="post.description" />
-
-      <PostMeta class="post-card__meta" :post="post" />
+    <div class="flex justify-between items-center">
+      <span class="font-light text-gray-600">
+        <PostMeta class="post-card__meta" :post="post" />
+      </span>
+    </div>
+    <div class="flex justify-between items-center mt-4">
       <PostTags class="post-card__tags" :post="post" />
-
-      <g-link class="post-card__link" :to="post.path">Link</g-link>
     </div>
+    <g-link class="post-card__link" :to="post.path">Link</g-link>
   </div>
 </template>
 
