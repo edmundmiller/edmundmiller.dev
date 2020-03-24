3de88ee853e7fe468e25b22bea16215de9b61b7f
Edmund Miller <edmund.a.miller@protonmail.com>
2020-03-23 22:42:31 -0500

    fix(ToggleTheme): change color on toggle

--- src/components/ToggleTheme.vue
+++ src/components/ToggleTheme.vue
@@ -16,7 +16,7 @@
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
-      class="feather feather-sun"
+      class="feather feather-sun fill-current text-body-color"
     >
       <circle cx="12" cy="12" r="5"></circle>
       <line x1="12" y1="1" x2="12" y2="3"></line>
@@ -39,7 +39,7 @@
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
-      class="feather feather-moon"
+      class="feather feather-moon fill-current text-body-color"
     >
       <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
     </svg>
