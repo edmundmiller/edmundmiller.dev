042f66f05e86afe0a833bbe6656b6ba6e8762469
Edmund Miller <edmund.a.miller@protonmail.com>
2020-03-23 23:05:46 -0500

    fix(Logo): change text color on theme switch

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,5 +1,5 @@
 <template>
-  <g-link class="logo no-underline bg-brand text-sm" to="/">
+  <g-link class="logo no-underline bg-brand text-sm text-link-color" to="/">
     <span class="logo__text align-middle rounded-lg h-40px w-40px mr-2">
       &larr; {{ $static.metadata.siteName }}
     </span>
