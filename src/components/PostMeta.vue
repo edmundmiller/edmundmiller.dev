61aece1ed4471107992f7324514b7a5b477952ee
Edmund Miller <edmund.a.miller@protonmail.com>
2020-03-25 21:59:54 -0500

    fix(PostMeta): add a transition to text color

--- src/components/PostMeta.vue
+++ src/components/PostMeta.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="post-meta text-sm text-title-color opacity-75">
+  <div
+    class="post-meta text-sm transition duration-700 text-title-color opacity-75"
+  >
     Posted {{ post.date }}.
     <template v-if="post.timeToRead">
       <strong>{{ post.timeToRead }} min read.</strong>
