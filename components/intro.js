c3c8360d6b5dc29a6bfaf67267d44aa0678b678b
Edmund Miller <edmund.a.miller@protonmail.com>
2022-05-08 21:48:16 -0500

    fix: specify file extension in all relative imports
    https://github.com/rasendubi/uniorg/commit/e3a36df36600245c38def599efe64f82cf4d3434

--- components/intro.js
+++ components/intro.js
@@ -1,10 +1,10 @@
-import { CMS_NAME } from '../lib/constants';
+import { CMS_NAME } from '../lib/constants.js';
 
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        Edmund Miller
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         A statically generated blog example using{' '}
