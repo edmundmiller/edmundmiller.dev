99bc8941b185e42d091070878c80b4d0cc742139
Edmund Miller <edmund.a.miller@protonmail.com>
2023-08-07 18:26:51 -0500

    Add publish.el
    https://github.com/maker2413/OrgPublish/blob/master/publish.el

--- publish.el
+++ publish.el
@@ -1,10 +1,20 @@
+(package-initialize)
+
+(require 'htmlize)
+(require 'ox-publish)
+
+;; TODO https://git.tecosaur.net/tec/this-month-in-org/src/branch/master/publish.el
+;; Define the publishing project
 (setq org-publish-project-alist
       (list
        (list "org-files"
              :recursive t
-             :base-directory "~/src/personal/edmundmiller-dev/src/"
+             :base-directory "./src"
              :html-validation-link nil             ;; Dont show validation link
-             :publishing-directory "~/src/personal/edmundmiller-dev/output/"
+             :publishing-directory "./html"
              :publishing-function 'org-html-publish-to-html
              :section-numbers nil                  ;; Don't show section numbers
              :with-author nil)))                   ;; Don't show author
+
+;; Generate the site output
+(org-publish-all t)
