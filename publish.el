cb4cb368fdf54043a2b69db2a63ec7f48189a349
Edmund Miller <edmund.a.miller@protonmail.com>
2023-08-09 12:00:21 -0500

    build: Reorganize publish alist
    https://taingram.org/blog/org-mode-blog.html

--- publish.el
+++ publish.el
@@ -6,15 +6,20 @@
 ;; TODO https://git.tecosaur.net/tec/this-month-in-org/src/branch/master/publish.el
 ;; Define the publishing project
 (setq org-publish-project-alist
-      (list
-       (list "org-files"
-             :recursive t
-             :base-directory "./src"
-             :html-validation-link nil             ;; Dont show validation link
-             :publishing-directory "./html"
-             :publishing-function 'org-html-publish-to-html
-             :section-numbers nil                  ;; Don't show section numbers
-             :with-author nil)))                   ;; Don't show author
+      `(("pages"
+         :recursive nil ;; avoid exporting blog twice
+         :base-directory "./src"
+         :html-validation-link nil             ;; Dont show validation link
+         :publishing-directory "./html"
+         :publishing-function org-html-publish-to-html
+         :section-numbers nil                  ;; Don't show section numbers
+         :with-author nil)                   ;; Don't show author
+        ("blog"
+         :base-directory "./src/posts"
+         :base-extension "org"
+         :publishing-directory "./html/posts"
+         :publishing-function org-html-publish-to-html)
+        ("edmundmiller.dev" :components ("pages" "blog"))))
 
 ;; Generate the site output
 (org-publish-all t)
