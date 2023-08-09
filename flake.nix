b0b6d2dc86517237e20e98ec084752a5dfd9427b
Edmund Miller <edmund.a.miller@protonmail.com>
2023-08-09 13:21:08 -0500

    style: Run alejandra

--- flake.nix
+++ flake.nix
@@ -1,11 +1,15 @@
 {
   description = "Personal Website of Edmund Miller";
 
-  outputs = { self, nixpkgs }: {
-    devShell.x86_64-linux =
-      let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in pkgs.mkShell {
-        buildInputs = with pkgs;  [
+  outputs = {
+    self,
+    nixpkgs,
+  }: {
+    devShell.x86_64-linux = let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+      pkgs.mkShell {
+        buildInputs = with pkgs; [
           nodejs-16_x
           nodePackages.typescript-language-server
         ];
