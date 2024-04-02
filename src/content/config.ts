2f3dddb357004c37aad40f2208b3f67bdf7d26bc
Edmund Miller <git@edmundmiller.dev>
2024-04-02 12:17:56 -0500

    Initial commit (via bun create)
    bun create chrismwilliams/astro-theme-cactus

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,36 @@
-import { defineCollection, z } from 'astro:content';
+import { z, defineCollection } from "astro:content";
 
-const blog = defineCollection({
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    date: z.coerce.date(),
-    last_modified: z.coerce.date().optional(),
-    hero_image: z.string().optional(),
-  }),
+function removeDupsAndLowerCase(array: string[]) {
+	if (!array.length) return array;
+	const lowercaseItems = array.map((str) => str.toLowerCase());
+	const distinctItems = new Set(lowercaseItems);
+	return Array.from(distinctItems);
+}
+
+const post = defineCollection({
+	type: "content",
+	schema: ({ image }) =>
+		z.object({
+			title: z.string().max(60),
+			description: z.string().min(50).max(160),
+			publishDate: z
+				.string()
+				.or(z.date())
+				.transform((val) => new Date(val)),
+			updatedDate: z
+				.string()
+				.optional()
+				.transform((str) => (str ? new Date(str) : undefined)),
+			coverImage: z
+				.object({
+					src: image(),
+					alt: z.string(),
+				})
+				.optional(),
+			draft: z.boolean().default(false),
+			tags: z.array(z.string()).default([]).transform(removeDupsAndLowerCase),
+			ogImage: z.string().optional(),
+		}),
 });
 
-export const collections = { blog };
+export const collections = { post };
