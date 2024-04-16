19705c438180e18b21ee41c5835c55bcba0d75b8
Edmund Miller <git@edmundmiller.dev>
2024-04-15 21:49:15 -0500

    refactor: Write Openring to json to use in Astro Component
    Because why not?
    Had to comment out the blogs that couldn't play nice.

--- .github/openring.sh
+++ .github/openring.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 openring \
-  -s https://drewdevault.com/blog/index.xml \
   -s https://monimiller.com/rss.xml \
-  -s https://taehoonkim.org/news/?format=rss \
-  <./.github/openring.html \
-  >./src/components/openring-out.html
+  \
+  -s https://www.nextflow.io/feed.xml \
+  -s https://nf-co.re/blog/rss.xml \
+  <./.github/openring.json.tmpl \
+  >./src/data/openring-out.json # -s https://taehoonkim.org/news/?format=rss \
+# -s https://drewdevault.com/blog/index.xml \
---
+++ src/content/post/openring-astro.md
@@ -0,0 +1,9 @@
+---
+title: Creating Webrings with Openring in Astro
+description: Yet another blog post on over engineering my website to replace social media.
+publishDate: 2024-04-15
+tags: ["Indieweb"]
+draft: true
+---
+
+Inspiration: https://www.thedroneely.com/posts/webrings-with-openring/
