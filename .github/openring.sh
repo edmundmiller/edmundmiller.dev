da45177e58a9cc9d4478285acd0da1481c864a95
Edmund Miller <git@edmundmiller.dev>
2024-04-16 08:06:13 -0500

    ci: Add a few more blogs

--- .github/openring.sh
+++ .github/openring.sh
@@ -5,6 +5,8 @@ openring \
   \
   -s https://www.nextflow.io/feed.xml \
   -s https://nf-co.re/blog/rss.xml \
+  -s https://blog.tecosaur.com/tmio/rss.xml \
+  -s https://xeiaso.net/blog.rss \
   <./.github/openring.json.tmpl \
   >./src/data/openring-out.json # -s https://taehoonkim.org/news/?format=rss \
 # -s https://drewdevault.com/blog/index.xml \
