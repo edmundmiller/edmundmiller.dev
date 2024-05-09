253b850f82d584ab49a96c29b9f3ad14df553243
Edmund Miller <git@edmundmiller.dev>
2024-05-09 17:39:23 -0500

    ci: Move comment down

--- .github/openring.sh
+++ .github/openring.sh
@@ -6,7 +6,8 @@ openring \
   -s https://www.nextflow.io/feed.xml \
   -s https://nf-co.re/blog/rss.xml \
   -s https://blog.tecosaur.com/tmio/rss.xml \
-  # FIXME -s https://xeiaso.net/blog.rss \
   <./.github/openring.json.tmpl \
   >./src/data/openring-out.json # -s https://taehoonkim.org/news/?format=rss \
+# Graveyard
 # -s https://drewdevault.com/blog/index.xml \
+# FIXME -s https://xeiaso.net/blog.rss \
