bb0788659d58a543704427afbef27bf44c21ced9
Edmund Miller <git@edmundmiller.dev>
2024-05-22 10:39:36 -0400

    fix: Convert openring output from .json => .json.ts
    So that multiline summaries don't keep breaking my builds

--- .github/openring.sh
+++ .github/openring.sh
@@ -2,12 +2,11 @@
 
 openring \
   -s https://monimiller.com/rss.xml \
-  \
   -s https://www.nextflow.io/feed.xml \
   -s https://nf-co.re/blog/rss.xml \
   -s https://blog.tecosaur.com/tmio/rss.xml \
-  <./.github/openring.json.tmpl \
-  >./src/data/openring-out.json # -s https://taehoonkim.org/news/?format=rss \
-# Graveyard
-# -s https://drewdevault.com/blog/index.xml \
-# FIXME -s https://xeiaso.net/blog.rss \
+  -s https://taehoonkim.org/news/?format=rss \
+  -s https://drewdevault.com/blog/index.xml \
+  -s https://xeiaso.net/blog.rss \
+  <./.github/openring.json.ts.tmpl \
+  >./src/data/openring-out.json.ts
--- src/data/openring-out.json.ts
+++ src/data/openring-out.json.ts
@@ -1,9 +1,19 @@
+export interface OpenringItem {
+  title: string;
+  link: string;
+  summary: string;
+  source_title: string;
+  source_link: string;
+  date: string;
+}
+
+export const openringData: OpenringItem[] =
 [
   
   {
     "title": "Open call for new Nextflow Ambassadors closes June 14",
     "link": "https://nextflow.io/blog/2024/ambassador-second-call.html",
-    "summary": "We&amp;#39;ve been recruiting more volunteers to the Nextflow Ambasador program and the applications close in June 14. This post is an official reminder for those interested in applying! ;)",
+    "summary": `We&amp;#39;ve been recruiting more volunteers to the Nextflow Ambasador program and the applications close in June 14. This post is an official reminder for those interested in applying! ;)`,
     "source_title": "Nextflow Blog",
     "source_link": "https://nextflow.io/",
     "date": "May 17, 2024"
@@ -12,7 +22,7 @@
   {
     "title": "Monica vs the Machine - Women&#39;s 2023 World Cup Analysis",
     "link": "https://monimiller.com/blog/womens-wc-23/womens-wc-23-introduction/",
-    "summary": "Pitting myself versus a CatBoost algorithm to see who wins.",
+    "summary": `Pitting myself versus a CatBoost algorithm to see who wins.`,
     "source_title": "Monica Miller - Data Girl in a Data World",
     "source_link": "https://monimiller.com/",
     "date": "July 20, 2023"
@@ -21,14 +31,16 @@
   {
     "title": "May 2022",
     "link": "https://blog.tecosaur.com/tmio/2022-05-31-folding.html",
-    "summary": "
+    "summary": `
     Finding time as of late has been more difficult than I anticipated, and on top
     of that, just as I was thinking of writing last month&amp;#39;s post, I got distracted
     by an exciting patchset that has been in the works for over a year finally
-    getti…",
+    getti…`,
     "source_title": "This Month in Org",
     "source_link": "https://blog.tecosaur.com/tmio/",
     "date": "June 2, 2022"
   },
   
-]
+];
+
+export default openringData;
