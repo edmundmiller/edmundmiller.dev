aaa43fd746f2c7d58567dc1c2f942f8187c120b9
edmundmiller <edmundmiller@users.noreply.github.com>
2024-06-17 01:08:32 +0000

    Run Openring

--- src/data/openring-out.json.ts
+++ src/data/openring-out.json.ts
@@ -11,12 +11,21 @@ export const openringData: OpenringItem[] =
 [
   
   {
-    "title": "Overengineering this blog&#39;s preview site with Kubernetes",
-    "link": "https://xeiaso.net/blog/2024/overengineering-preview-site/",
-    "summary": `A small overview on how future-sight, my blog&amp;#39;s preview site server, is overengineered with the power of Kubernetes.`,
+    "title": "Function calling in large language models",
+    "link": "https://xeiaso.net/talks/2024/llm-function-calling/",
+    "summary": `It&amp;#39;s hard to pay attention when your context goes out the window.`,
     "source_title": "Xe Iaso&#39;s blog",
     "source_link": "https://xeiaso.net/",
-    "date": "June 9, 2024"
+    "date": "June 16, 2024"
+  },
+  
+  {
+    "title": "Fostering Bioinformatics Growth in Türkiye",
+    "link": "https://nextflow.io/blog/2024/bioinformatics-growth-in-turkiye.html",
+    "summary": `Kübra shares her first steps towards the establishment of a Nextflow community in Türkiye!`,
+    "source_title": "Nextflow Blog",
+    "source_link": "https://nextflow.io/",
+    "date": "June 12, 2024"
   },
   
   {
@@ -26,16 +35,6 @@ export const openringData: OpenringItem[] =
     "source_title": "Monica Miller - Data Girl in a Data World",
     "source_link": "https://monimiller.com/",
     "date": "May 29, 2024"
-  },
-  
-  {
-    "title": "Nextflow 24.04 - Release highlights",
-    "link": "https://nextflow.io/blog/2024/nextflow-2404-highlights.html",
-    "summary": `A highlight of some of the goodies that have just been released in the 24.04 stable release of Nextflow 24.04 stable.
-`,
-    "source_title": "Nextflow Blog",
-    "source_link": "https://nextflow.io/",
-    "date": "May 27, 2024"
   },
   
 ];
