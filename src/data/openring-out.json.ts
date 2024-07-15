ab93b300319e8e33d4d41f2e7958d39e2084009f
edmundmiller <edmundmiller@users.noreply.github.com>
2024-07-15 01:10:12 +0000

    Run Openring

--- src/data/openring-out.json.ts
+++ src/data/openring-out.json.ts
@@ -11,32 +11,30 @@ export const openringData: OpenringItem[] =
 [
   
   {
-    "title": "&#34;No way to prevent this&#34; say users of only language where this regularly happens",
-    "link": "https://xeiaso.net/shitposts/no-way-to-prevent-this/CVE-2024-6387/",
-    "summary": `
-        In the hours following the release of CVE-2024-6387 for the project OpenSSH, site reliability workers
-        and systems administrators scrambled to desperately rebuild and patch all their systems to fix a combination of memory unsafety and glibc&amp;…`,
-    "source_title": "Xe Iaso&#39;s blog",
-    "source_link": "https://xeiaso.net/",
-    "date": "July 1, 2024"
-  },
-  
-  {
-    "title": "Reflecting on a Six-Month Collaboration: Insights from a Nextflow Ambassador",
-    "link": "https://nextflow.io/blog/2024/reflecting-ambassador-collaboration.html",
-    "summary": `Cristina Tuñi reflects on a collaboration that emerged after her contact was found in the Nextflow Ambassador page.`,
+    "title": "Join us in welcoming the new Nextflow Ambassadors",
+    "link": "https://nextflow.io/blog/2024/welcome_ambassadors_20242.html",
+    "summary": `It&amp;#39;s time for a new cohort of Nextflow Ambassadors, with both new and continuing volunteers, to start!`,
     "source_title": "Nextflow Blog",
     "source_link": "https://nextflow.io/",
-    "date": "June 19, 2024"
+    "date": "July 10, 2024"
   },
   
   {
-    "title": "Can I still be a data engineer if I don’t know Python?",
-    "link": "https://monimiller.com/blog/should-my-data-engineering-title-be-revoked-if-i-dont-know-python/",
-    "summary": `A self-discovery into life, liberty, and the pursuit of data.`,
-    "source_title": "Monica Miller - Data Girl in a Data World",
-    "source_link": "https://monimiller.com/",
-    "date": "May 29, 2024"
+    "title": "My fears about AI are not what you think",
+    "link": "https://xeiaso.net/videos/2024/ai-fears/",
+    "summary": `A clip from a longer stream VOD where I run through my fears with the AI industry`,
+    "source_title": "Xe Iaso&#39;s blog",
+    "source_link": "https://xeiaso.net/",
+    "date": "July 8, 2024"
+  },
+  
+  {
+    "title": "Maintainers Minutes: July 2024",
+    "link": "https://nf-co.re/blog/2024/maintainers-minutes-2024-07-02/",
+    "summary": `Keeping you informed of the latest maintainers discussions`,
+    "source_title": "nf-core blog",
+    "source_link": "https://nf-co.re/",
+    "date": "July 3, 2024"
   },
   
 ];
