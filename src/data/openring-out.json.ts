e663c6c2d1a70d03d75a720c3dbd85dd4d69fa4e
edmundmiller <edmundmiller@users.noreply.github.com>
2024-08-26 01:11:16 +0000

    Run Openring

--- src/data/openring-out.json.ts
+++ src/data/openring-out.json.ts
@@ -11,22 +11,20 @@ export const openringData: OpenringItem[] =
 [
   
   {
+    "title": "Xecast Episode 3: The curse of the artist",
+    "link": "https://xeiaso.net/xecast/003/",
+    "summary": `Xe returns while on vacation where they built a new PC, made a SaaS to check web server headers, and re-evaluated how they think about complexity.`,
+    "source_title": "Xe Iaso&#39;s blog",
+    "source_link": "https://xeiaso.net/",
+    "date": "August 25, 2024"
+  },
+  
+  {
     "title": "Maintainers Minutes: August 2024",
     "link": "https://nf-co.re/blog/2024/maintainers-minutes-2024-07-26/",
     "summary": `Keeping you informed of the latest maintainers discussions`,
     "source_title": "nf-core blog",
     "source_link": "https://nf-co.re/",
-    "date": "August 14, 2024"
-  },
-  
-  {
-    "title": "&#34;No way to prevent this&#34; say users of only language where this regularly happens",
-    "link": "https://xeiaso.net/shitposts/no-way-to-prevent-this/CVE-2024-38063/",
-    "summary": `
-        In the hours following the release of CVE-2024-38063 for the project Microsoft Windows, site reliability workers
-        and systems administrators scrambled to desperately rebuild and patch all their systems to fix a vulnerability where a speciall…`,
-    "source_title": "Xe Iaso&#39;s blog",
-    "source_link": "https://xeiaso.net/",
     "date": "August 14, 2024"
   },
   
