8b607eaf5bea890c2bdc01503e890cc3d47001a6
edmundmiller <edmundmiller@users.noreply.github.com>
2024-11-18 01:23:22 +0000

    Run Openring

--- src/data/openring-out.json.ts
+++ src/data/openring-out.json.ts
@@ -11,12 +11,12 @@ export const openringData: OpenringItem[] =
 [
   
   {
-    "title": "Hello again, Kubernetes",
-    "link": "https://xeiaso.net/blog/2024/hello-again-k8s/",
-    "summary": `Yeah, yeah, we know; freight train to mail a letter, etc.`,
+    "title": "Follow me on Bluesky!",
+    "link": "https://xeiaso.net/notes/2024/follow-bluesky/",
+    "summary": `tl;dr: Follow @xeiaso.net on Bluesky`,
     "source_title": "Xe Iaso&#39;s blog",
     "source_link": "https://xeiaso.net/",
-    "date": "November 9, 2024"
+    "date": "November 13, 2024"
   },
   
   {
