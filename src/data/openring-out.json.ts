91df234812bf6c43526f38a885b85ce99593d8f2
edmundmiller <20095261+edmundmiller@users.noreply.github.com>
2025-04-14 01:27:48 +0000

    Run Openring

--- src/data/openring-out.json.ts
+++ src/data/openring-out.json.ts
@@ -11,33 +11,58 @@ export const openringData: OpenringItem[] =
 [
   
   {
-    "title": "Life pro tip: put your active kubernetes context in your prompt",
-    "link": "https://xeiaso.net/notes/2025/kube-ps1/",
-    "summary": `kube_ps1 is love, kube_ps1 is life`,
+    "title": "Anubis works",
+    "link": "https://xeiaso.net/notes/2025/anubis-works/",
+    "summary": `
+        That meme is not an understatement, Anubis has been deployed by the United Nations.
+        For your amusement, here is how the inner monologue of me finding out about this went:
+        AoiWhat. You can&amp;#39;t be serious, can you?CadeyIt&amp;#39;s real.…`,
     "source_title": "Xe Iaso&#39;s blog",
     "source_link": "https://xeiaso.net/",
-    "date": "April 5, 2025"
+    "date": "April 12, 2025"
   },
   
   {
-    "title": "A Firefox addon for putting prices into perspective",
-    "link": "https://drewdevault.com/2025/04/04/2025-04-04-Price-perspective.html",
-    "summary": `I had a fun idea for a small project this weekend, and so I quickly put it
-together over the couple of days. The result is Price Perspective.
-Humor me: have you ever bought something, considered the price, and wondered how
-that price would look to someone el…`,
-    "source_title": "Drew DeVault&#39;s blog",
-    "source_link": "https://drewdevault.com",
-    "date": "April 4, 2025"
+    "title": "Jacob Roy story of his UTD journey and giving",
+    "link": "https://taehoonkim.org/news/2025/4/10/jacob-roy-story-of-utd-journey-and-giving",
+    "summary": `
+          
+        
+        
+
+        
+          
+            
+          
+            
+                
+                
+                
+                
+                
+                
+                
+                
+
+            
+          
+        
+          
+        
+
+ …`,
+    "source_title": "News - Functional Genomics Laboratory",
+    "source_link": "https://taehoonkim.org/news/",
+    "date": "April 10, 2025"
   },
   
   {
-    "title": "nf-core survey 2025: the results",
-    "link": "https://nf-co.re/blog/2025/survey-results-2025/",
-    "summary": `Results summary from the nf-core community survey 2025 `,
+    "title": "State of the nf-core CI",
+    "link": "https://nf-co.re/blog/2025/state-of-nf-core-ci/",
+    "summary": `Run, CI, Run!`,
     "source_title": "nf-core blog",
     "source_link": "https://nf-co.re/",
-    "date": "March 26, 2025"
+    "date": "April 9, 2025"
   },
   
 ];
