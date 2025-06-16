0c79d927ac5447bf58d3ae7981a68957ccc18914
edmundmiller <20095261+edmundmiller@users.noreply.github.com>
2025-06-16 01:45:24 +0000

    Run Openring

--- src/data/openring-out.json.ts
+++ src/data/openring-out.json.ts
@@ -11,58 +11,34 @@ export const openringData: OpenringItem[] =
 [
   
   {
+    "title": "I fight bots in my free time",
+    "link": "https://xeiaso.net/talks/2025/bsdcan-anubis/",
+    "summary": `
+        I made Anubis, a lightweight, open-source Web Application Firewall designed to block automated bot attacks without frustrating real users. Anubis uses techniques like TLS fingerprinting and JavaScript proof of work to keep your site safe while sta…`,
+    "source_title": "Xe Iaso&#39;s blog",
+    "source_link": "https://xeiaso.net/",
+    "date": "June 15, 2025"
+  },
+  
+  {
+    "title": "Unionize or die",
+    "link": "https://drewdevault.com/2025/06/09/2025-06-09-Unionize-or-die.html",
+    "summary": `Tech workers have long resisted the suggestion that we should be organized into
+unions. The topic is consistently met with a cold reception by tech workers when
+it is raised, and no big tech workforce is meaningfully organized. This is a
+fatal mistake – and …`,
+    "source_title": "Drew DeVault&#39;s blog",
+    "source_link": "https://drewdevault.com",
+    "date": "June 9, 2025"
+  },
+  
+  {
     "title": "nf-core/tools - 3.3",
     "link": "https://nf-co.re/blog/2025/tools-3_3/",
     "summary": `An nf-testic release`,
     "source_title": "nf-core blog",
     "source_link": "https://nf-co.re/",
     "date": "June 3, 2025"
-  },
-  
-  {
-    "title": "cytotoxic oxidative stress for cancer therapy",
-    "link": "https://taehoonkim.org/news/2025/5/28/k5lcl7cascox9hy20wtkhz4721jksx",
-    "summary": `
-          
-        
-        
-
-        
-          
-            
-          
-            
-                
-                
-                
-                
-                
-                
-                
-                
-
-            
-          
-        
-          
-        
-
- …`,
-    "source_title": "News - Functional Genomics Laboratory",
-    "source_link": "https://taehoonkim.org/news/",
-    "date": "May 28, 2025"
-  },
-  
-  {
-    "title": "Avoiding becoming the lone dependency peg with load-bearing anime",
-    "link": "https://xeiaso.net/blog/2025/avoiding-becoming-peg-dependency/",
-    "summary": `
-        While working on Anubis (a Web AI Firewall Utility designed to stop rampant scraping from taking out web services), one question in particular keeps coming up:
-        AoiWhy do you have an anime character in the challenge screen by default?
-       …`,
-    "source_title": "Xe Iaso&#39;s blog",
-    "source_link": "https://xeiaso.net/",
-    "date": "May 23, 2025"
   },
   
 ];
