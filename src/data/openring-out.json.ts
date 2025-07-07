435ee34a51313b3d29a915e55330bf4df7321e98
edmundmiller <20095261+edmundmiller@users.noreply.github.com>
2025-07-07 01:48:07 +0000

    Run Openring

--- src/data/openring-out.json.ts
+++ src/data/openring-out.json.ts
@@ -11,24 +11,27 @@ export const openringData: OpenringItem[] =
 [
   
   {
+    "title": "Just speak the truth",
+    "link": "https://drewdevault.com/2025/06/30/Speak-the-truth.html",
+    "summary": `Today, we’re looking at two case studies in how to respond when reactionaries
+appear in your free software community.
+Exhibit A
+
+It is a technical decision.
+The technical reason is that the security team does not have the bandwidth to
+provide lifecycle maintena…`,
+    "source_title": "Drew DeVault&#39;s blog",
+    "source_link": "https://drewdevault.com",
+    "date": "June 30, 2025"
+  },
+  
+  {
     "title": "Experimenting with Development containers",
     "link": "https://xeiaso.net/notes/2025/devcontainers/",
     "summary": `Development containers are cool and I want to see if they work out in practice.`,
     "source_title": "Xe Iaso&#39;s blog",
     "source_link": "https://xeiaso.net/",
     "date": "June 30, 2025"
-  },
-  
-  {
-    "title": "Unionize or die",
-    "link": "https://drewdevault.com/2025/06/09/2025-06-09-Unionize-or-die.html",
-    "summary": `Tech workers have long resisted the suggestion that we should be organized into
-unions. The topic is consistently met with a cold reception by tech workers when
-it is raised, and no big tech workforce is meaningfully organized. This is a
-fatal mistake – and …`,
-    "source_title": "Drew DeVault&#39;s blog",
-    "source_link": "https://drewdevault.com",
-    "date": "June 9, 2025"
   },
   
   {
