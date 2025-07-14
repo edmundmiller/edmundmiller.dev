9913982c5bd45b66397e650f3ffc2d2d71e98bab
edmundmiller <20095261+edmundmiller@users.noreply.github.com>
2025-07-14 01:50:04 +0000

    Run Openring

--- src/data/openring-out.json.ts
+++ src/data/openring-out.json.ts
@@ -11,6 +11,25 @@ export const openringData: OpenringItem[] =
 [
   
   {
+    "title": "TI-20250709-0001: IPv4 traffic failures for Techaro services",
+    "link": "https://anubis.techaro.lol/blog/incident/TI-20250709-0001",
+    "summary": `
+        Techaro services were down for IPv4 traffic on July 9th, 2025. This blogpost is a report of what happened, what actions were taken to resolve the situation, and what actions are being done in the near future to prevent this problem. Enjoy this inc…`,
+    "source_title": "Xe Iaso&#39;s blog",
+    "source_link": "https://xeiaso.net/",
+    "date": "July 9, 2025"
+  },
+  
+  {
+    "title": "Meet the new meta.yml",
+    "link": "https://nf-co.re/blog/2025/modules-meta-yml/",
+    "summary": `Changes to the structure of the meta.yml file for nf-core modules`,
+    "source_title": "nf-core blog",
+    "source_link": "https://nf-co.re/",
+    "date": "July 7, 2025"
+  },
+  
+  {
     "title": "Just speak the truth",
     "link": "https://drewdevault.com/2025/06/30/Speak-the-truth.html",
     "summary": `Today, we’re looking at two case studies in how to respond when reactionaries
@@ -23,24 +42,6 @@ provide lifecycle maintena…`,
     "source_title": "Drew DeVault&#39;s blog",
     "source_link": "https://drewdevault.com",
     "date": "June 30, 2025"
-  },
-  
-  {
-    "title": "Experimenting with Development containers",
-    "link": "https://xeiaso.net/notes/2025/devcontainers/",
-    "summary": `Development containers are cool and I want to see if they work out in practice.`,
-    "source_title": "Xe Iaso&#39;s blog",
-    "source_link": "https://xeiaso.net/",
-    "date": "June 30, 2025"
-  },
-  
-  {
-    "title": "nf-core/tools - 3.3",
-    "link": "https://nf-co.re/blog/2025/tools-3_3/",
-    "summary": `An nf-testic release`,
-    "source_title": "nf-core blog",
-    "source_link": "https://nf-co.re/",
-    "date": "June 3, 2025"
   },
   
 ];
