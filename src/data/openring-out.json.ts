c870585a9f93066b291712e233fd6692b470f76a
edmundmiller <20095261+edmundmiller@users.noreply.github.com>
2025-09-01 01:48:02 +0000

    Run Openring

--- src/data/openring-out.json.ts
+++ src/data/openring-out.json.ts
@@ -11,21 +11,21 @@ export const openringData: OpenringItem[] =
 [
   
   {
+    "title": "Why are my pipeline download tests failing?",
+    "link": "https://nf-co.re/blog/2025/refurbushing-the-pipeline-download/",
+    "summary": `Refurbishing the pipeline downloads command`,
+    "source_title": "nf-core blog",
+    "source_link": "https://nf-co.re/",
+    "date": "August 28, 2025"
+  },
+  
+  {
     "title": "Final Fantasy 14 on macOS with a 36 key keyboard",
     "link": "https://xeiaso.net/notes/2025/xiv-mac/",
     "summary": `Saving Eorzea with as few keys as possible`,
     "source_title": "Xe Iaso&#39;s blog",
     "source_link": "https://xeiaso.net/",
     "date": "August 24, 2025"
-  },
-  
-  {
-    "title": "Introducing nf-core Advisories",
-    "link": "https://nf-co.re/blog/2025/advisories/",
-    "summary": `Timely technical issue alerts, no matter how remote you are.`,
-    "source_title": "nf-core blog",
-    "source_link": "https://nf-co.re/",
-    "date": "August 21, 2025"
   },
   
   {
