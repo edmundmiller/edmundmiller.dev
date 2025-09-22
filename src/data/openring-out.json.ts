eef2798e7eb0e21d8945fb37645408248ea6a342
edmundmiller <20095261+edmundmiller@users.noreply.github.com>
2025-09-22 00:24:22 +0000

    Run Openring

--- src/data/openring-out.json.ts
+++ src/data/openring-out.json.ts
@@ -11,6 +11,18 @@ export const openringData: OpenringItem[] =
 [
   
   {
+    "title": "A better future for JavaScript that won&#39;t happen",
+    "link": "https://drewdevault.com/2025/09/17/2025-09-17-An-impossible-future-for-JS.html",
+    "summary": `In the wake of the largest supply-chain attack in history, the JavaScript
+community could have a moment of reckoning and decide: never again. As the panic
+and shame subsides, after compromised developers finish re-provisioning their
+workstations and rotating…`,
+    "source_title": "Drew DeVault&#39;s blog",
+    "source_link": "https://drewdevault.com",
+    "date": "September 17, 2025"
+  },
+  
+  {
     "title": "We all dodged a bullet",
     "link": "https://xeiaso.net/notes/2025/we-dodged-a-bullet/",
     "summary": `That NPM attack could have been so much worse.`,
@@ -25,15 +37,6 @@ export const openringData: OpenringItem[] =
     "summary": `Come November 20th, it will be 19 full years since I started my lab. It has been an amazing journey for me. Mentoring amidst the intense scientific competition has not been easy, but I am so proud of the work that my lab has contributed to the world. Many…`,
     "source_title": "News - Functional Genomics Laboratory",
     "source_link": "https://taehoonkim.org/news/",
-    "date": "September 2, 2025"
-  },
-  
-  {
-    "title": "Running nf-core pipelines on Google Colab",
-    "link": "https://nf-co.re/blog/2025/nf-core-colab-guide/",
-    "summary": `A guide to running and interacting with nf-core pipelines using Colab and VS Code`,
-    "source_title": "nf-core blog",
-    "source_link": "https://nf-co.re/",
     "date": "September 2, 2025"
   },
   
