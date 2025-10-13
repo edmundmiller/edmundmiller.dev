13ec880a700cb8869df0dddbf25670dabfd2ca01
edmundmiller <20095261+edmundmiller@users.noreply.github.com>
2025-10-13 00:24:01 +0000

    Run Openring

--- src/data/openring-out.json.ts
+++ src/data/openring-out.json.ts
@@ -11,6 +11,15 @@ export const openringData: OpenringItem[] =
 [
   
   {
+    "title": "successful doctoral defense for Edmund!",
+    "link": "https://taehoonkim.org/news/2025/10/7/successful-doctoral-defense-for-edmund",
+    "summary": `Edmund joined our lab in 2015, just as he was about to start his first year as a UTD undergraduate student. His research interests evolved from bench science and biomedical engineering to computational biology and bioinformatics. He has now completed BS, …`,
+    "source_title": "News - Functional Genomics Laboratory",
+    "source_link": "https://taehoonkim.org/news/",
+    "date": "October 2, 2025"
+  },
+  
+  {
     "title": "Hastily made coffee video",
     "link": "https://xeiaso.net/videos/2025/hastily-made-coffee-video/",
     "summary": `I made a video about making coffee`,
@@ -29,15 +38,6 @@ habeas corpus, exerting stat…`,
     "source_title": "Drew DeVault&#39;s blog",
     "source_link": "https://drewdevault.com",
     "date": "September 24, 2025"
-  },
-  
-  {
-    "title": "Gratitude for nearly 19 years and counting",
-    "link": "https://taehoonkim.org/news/2025/9/2/gratitude-for-nearly-19-years-and-counting",
-    "summary": `Come November 20th, it will be 19 full years since I started my lab. It has been an amazing journey for me. Mentoring amidst the intense scientific competition has not been easy, but I am so proud of the work that my lab has contributed to the world. Many…`,
-    "source_title": "News - Functional Genomics Laboratory",
-    "source_link": "https://taehoonkim.org/news/",
-    "date": "September 2, 2025"
   },
   
 ];
