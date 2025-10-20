c754c22a54855af13787754e1d0ee4727ea686f0
edmundmiller <20095261+edmundmiller@users.noreply.github.com>
2025-10-20 00:24:15 +0000

    Run Openring

--- src/data/openring-out.json.ts
+++ src/data/openring-out.json.ts
@@ -11,33 +11,30 @@ export const openringData: OpenringItem[] =
 [
   
   {
+    "title": "nf-core/tools - 3.4.0",
+    "link": "https://nf-co.re/blog/2025/tools-3_4_0/",
+    "summary": `Container regex-it`,
+    "source_title": "nf-core blog",
+    "source_link": "https://nf-co.re/",
+    "date": "October 16, 2025"
+  },
+  
+  {
+    "title": "First look at the DGX Spark",
+    "link": "https://xeiaso.net/blog/2025/dgx-spark-first-look/",
+    "summary": `A local supercomputer between the size of a Mac mini and a Mac mini.`,
+    "source_title": "Xe Iaso&#39;s blog",
+    "source_link": "https://xeiaso.net/",
+    "date": "October 14, 2025"
+  },
+  
+  {
     "title": "successful doctoral defense for Edmund!",
     "link": "https://taehoonkim.org/news/2025/10/7/successful-doctoral-defense-for-edmund",
     "summary": `Edmund joined our lab in 2015, just as he was about to start his first year as a UTD undergraduate student. His research interests evolved from bench science and biomedical engineering to computational biology and bioinformatics. He has now completed BS, …`,
     "source_title": "News - Functional Genomics Laboratory",
     "source_link": "https://taehoonkim.org/news/",
     "date": "October 2, 2025"
-  },
-  
-  {
-    "title": "Hastily made coffee video",
-    "link": "https://xeiaso.net/videos/2025/hastily-made-coffee-video/",
-    "summary": `I made a video about making coffee`,
-    "source_title": "Xe Iaso&#39;s blog",
-    "source_link": "https://xeiaso.net/",
-    "date": "September 30, 2025"
-  },
-  
-  {
-    "title": "Cloudflare bankrolls fascists",
-    "link": "https://drewdevault.com/2025/09/24/2025-09-24-Cloudflare-and-fascists.html",
-    "summary": `US politics has been pretty fascist lately. The state is filling up
-concentration camps, engaging in mass state violence against people on the basis
-of racialized traits, deporting them to random countries without any respect for
-habeas corpus, exerting stat…`,
-    "source_title": "Drew DeVault&#39;s blog",
-    "source_link": "https://drewdevault.com",
-    "date": "September 24, 2025"
   },
   
 ];
