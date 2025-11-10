f130c11db54e170a55ad8856e357ebee66786301
edmundmiller <20095261+edmundmiller@users.noreply.github.com>
2025-11-10 00:24:54 +0000

    Run Openring

--- src/data/openring-out.json.ts
+++ src/data/openring-out.json.ts
@@ -11,24 +11,25 @@ export const openringData: OpenringItem[] =
 [
   
   {
-    "title": "Using Assisted-by commit footers instead of banning AI tools",
-    "link": "https://xeiaso.net/notes/2025/assisted-by-footer/",
-    "summary": `It sounds like ceding ground to the pro-AI crowd, however if you get people to be honest about it then everyone benefits.`,
-    "source_title": "Xe Iaso&#39;s blog",
-    "source_link": "https://xeiaso.net/",
-    "date": "November 2, 2025"
+    "title": "OpenAI employees… are you okay?",
+    "link": "https://drewdevault.com/2025/11/08/2025-11-08-OpenAI-employees-are-you-okay.html",
+    "summary": `You might have seen an article making the rounds this week, about a young
+man who ended his life after ChatGPT encouraged him to do so. The chat logs are
+really upsetting.
+Someone two degrees removed from me took their life a few weeks ago. A close
+friend rel…`,
+    "source_title": "Drew DeVault&#39;s blog",
+    "source_link": "https://drewdevault.com",
+    "date": "November 8, 2025"
   },
   
   {
-    "title": "What&#39;s up with FUTO?",
-    "link": "https://drewdevault.com/2025/10/22/2025-10-22-Whats-up-with-FUTO.html",
-    "summary": `Some time ago, I noticed some new organization called FUTO popping up here and
-there. I’m always interested in seeing new organizations that fund open source
-popping up, and seeing as they claim several notable projects on their roster, I
-explored their webs…`,
-    "source_title": "Drew DeVault&#39;s blog",
-    "source_link": "https://drewdevault.com",
-    "date": "October 22, 2025"
+    "title": "Immutable by Design: The Deep Tech Behind Tigris Bucket Forking",
+    "link": "https://www.tigrisdata.com/blog/bucket-forking-deep-dive/",
+    "summary": ``,
+    "source_title": "Xe Iaso&#39;s blog",
+    "source_link": "https://xeiaso.net/",
+    "date": "November 4, 2025"
   },
   
   {
