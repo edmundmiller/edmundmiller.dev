c7b695200f47e7ed78279f432f70f72de375747b
edmundmiller <20095261+edmundmiller@users.noreply.github.com>
2025-11-24 00:25:30 +0000

    Run Openring

--- src/data/openring-out.json.ts
+++ src/data/openring-out.json.ts
@@ -11,34 +11,55 @@ export const openringData: OpenringItem[] =
 [
   
   {
+    "title": "nineteen and counting",
+    "link": "https://taehoonkim.org/news/2025/11/22/nineteen-and-counting",
+    "summary": `
+          
+        
+        
+
+        
+          
+            
+          
+            
+                
+                
+                
+                
+                
+                
+                
+                
+
+            
+          
+        
+          
+        
+
+ …`,
+    "source_title": "News - Functional Genomics Laboratory",
+    "source_link": "https://taehoonkim.org/news/",
+    "date": "November 20, 2025"
+  },
+  
+  {
+    "title": "nf-core/tools - 3.5.0",
+    "link": "https://nf-co.re/blog/2025/tools-3_5_0/",
+    "summary": `Hot topic release`,
+    "source_title": "nf-core blog",
+    "source_link": "https://nf-co.re/",
+    "date": "November 19, 2025"
+  },
+  
+  {
     "title": "Valve is about to win the console generation",
     "link": "https://xeiaso.net/blog/2025/valve-is-about-to-win-the-console-generation/",
     "summary": `Valve does nothing and still wins`,
     "source_title": "Xe Iaso&#39;s blog",
     "source_link": "https://xeiaso.net/",
     "date": "November 12, 2025"
-  },
-  
-  {
-    "title": "OpenAI employees… are you okay?",
-    "link": "https://drewdevault.com/2025/11/08/2025-11-08-OpenAI-employees-are-you-okay.html",
-    "summary": `You might have seen an article making the rounds this week, about a young
-man who ended his life after ChatGPT encouraged him to do so. The chat logs are
-really upsetting.
-Someone two degrees removed from me took their life a few weeks ago. A close
-friend rel…`,
-    "source_title": "Drew DeVault&#39;s blog",
-    "source_link": "https://drewdevault.com",
-    "date": "November 8, 2025"
-  },
-  
-  {
-    "title": "nf-core core team retreat - September 2025",
-    "link": "https://nf-co.re/blog/2025/retreat-2025/",
-    "summary": `Updates from the nf-core core team retreat.`,
-    "source_title": "nf-core blog",
-    "source_link": "https://nf-co.re/",
-    "date": "October 20, 2025"
   },
   
 ];
