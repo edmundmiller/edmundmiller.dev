6a83b48bfd6bf4a3a874df552a5da2954e0a269a
edmundmiller <20095261+edmundmiller@users.noreply.github.com>
2025-12-29 00:27:34 +0000

    Run Openring

--- src/data/openring-out.json.ts
+++ src/data/openring-out.json.ts
@@ -11,21 +11,21 @@ export const openringData: OpenringItem[] =
 [
   
   {
+    "title": "Arcane Cheese with Doomtrain Extreme",
+    "link": "https://xeiaso.net/notes/2025/doomtrain-ex-cheese/",
+    "summary": `If you don&amp;#39;t play Final Fantasy 14, try to identify how many words in this post are in the bible.`,
+    "source_title": "Xe Iaso&#39;s blog",
+    "source_link": "https://xeiaso.net/",
+    "date": "December 24, 2025"
+  },
+  
+  {
     "title": "Maintainers Minutes: November 2025",
     "link": "https://nf-co.re/blog/2025/maintainers-minutes-2025-11-28/",
     "summary": `Keeping you informed of the latest maintainers discussions`,
     "source_title": "nf-core blog",
     "source_link": "https://nf-co.re/",
     "date": "December 8, 2025"
-  },
-  
-  {
-    "title": "New AI slop signal: code blocks with weird indentation",
-    "link": "https://xeiaso.net/notes/2025/slop-signal-indentation/",
-    "summary": `If the first line of a set of commands isn&amp;#39;t indented but the rest are: the post is AI slop`,
-    "source_title": "Xe Iaso&#39;s blog",
-    "source_link": "https://xeiaso.net/",
-    "date": "December 1, 2025"
   },
   
   {
