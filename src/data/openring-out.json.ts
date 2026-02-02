7db480466cebb35c95b462dfb26b0375a3167897
edmundmiller <20095261+edmundmiller@users.noreply.github.com>
2026-02-02 00:31:22 +0000

    Run Openring

--- src/data/openring-out.json.ts
+++ src/data/openring-out.json.ts
@@ -11,12 +11,26 @@ export const openringData: OpenringItem[] =
 [
   
   {
-    "title": "Tormentmaxxing &#39;simple requests&#39;",
-    "link": "https://xeiaso.net/notes/2026/tormentmaxxing-simple-requests/",
-    "summary": `Automating the full lifecycle of &amp;#39;quick&amp;#39; requests with Claude Code slash commands`,
+    "title": "The cults of TDD and GenAI",
+    "link": "https://drewdevault.com/2026/01/29/2026-01-29-Cult-of-TDD-and-LLMs.html",
+    "summary": `I’ve gotten a lot of flack throughout my career over my disdain towards
+test-driven development (TDD). I have met a lot of people who swear by it! And,
+I have also met a lot of people who insisted that I adopt it, too, often with
+the implied threat of appeal…`,
+    "source_title": "Drew DeVault&#39;s blog",
+    "source_link": "https://drewdevault.com",
+    "date": "January 29, 2026"
+  },
+  
+  {
+    "title": "Backfilling Discord forum channels with the power of terrible code",
+    "link": "https://www.tigrisdata.com/blog/discord-backfill/",
+    "summary": `
+        Hey all! We&amp;#39;ve got a Discord so you can chat with us about the wild world of object storage and get any help you need. We&amp;#39;ve also set up Answer Overflow so that you can browse the Q&amp;amp;A from the web.
+        Today I&amp;#39;m going to discuss…`,
     "source_title": "Xe Iaso&#39;s blog",
     "source_link": "https://xeiaso.net/",
-    "date": "January 15, 2026"
+    "date": "January 27, 2026"
   },
   
   {
@@ -26,19 +40,6 @@ export const openringData: OpenringItem[] =
     "source_title": "nf-core blog",
     "source_link": "https://nf-co.re/",
     "date": "January 14, 2026"
-  },
-  
-  {
-    "title": "Redesigning my microkernel from the ground up",
-    "link": "https://drewdevault.com/2026/01/12/2026-01-12-Hermes-from-the-ground-up.html",
-    "summary": `As you may recall, circa 2022-2023 I was working on a
-microkernel written in Hare named Helios. Helios was largely inspired by and
-modelled after the design of seL4 and was my first
-major foray into modern OS development that was serious enough to get to a
-so…`,
-    "source_title": "Drew DeVault&#39;s blog",
-    "source_link": "https://drewdevault.com",
-    "date": "January 12, 2026"
   },
   
 ];
