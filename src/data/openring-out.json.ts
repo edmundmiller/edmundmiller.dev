ea8dabb509581a46a1273230bcea9814585a7eb6
edmundmiller <20095261+edmundmiller@users.noreply.github.com>
2026-02-09 00:32:16 +0000

    Run Openring

--- src/data/openring-out.json.ts
+++ src/data/openring-out.json.ts
@@ -11,6 +11,15 @@ export const openringData: OpenringItem[] =
 [
   
   {
+    "title": "Zendesk, get your shit together please",
+    "link": "https://xeiaso.net/notes/2026/zendesk-get-shit-together/",
+    "summary": `BORN TO GET SPAMMED, WORLD IS A FUCK, SPAMHAUS THEM ALL 2026, I am inbox full man, 410,757,864,530 Zendesk emails`,
+    "source_title": "Xe Iaso&#39;s blog",
+    "source_link": "https://xeiaso.net/",
+    "date": "February 7, 2026"
+  },
+  
+  {
     "title": "The cults of TDD and GenAI",
     "link": "https://drewdevault.com/2026/01/29/2026-01-29-Cult-of-TDD-and-LLMs.html",
     "summary": `I’ve gotten a lot of flack throughout my career over my disdain towards
@@ -20,17 +29,6 @@ the implied threat of appeal…`,
     "source_title": "Drew DeVault&#39;s blog",
     "source_link": "https://drewdevault.com",
     "date": "January 29, 2026"
-  },
-  
-  {
-    "title": "Backfilling Discord forum channels with the power of terrible code",
-    "link": "https://www.tigrisdata.com/blog/discord-backfill/",
-    "summary": `
-        Hey all! We&amp;#39;ve got a Discord so you can chat with us about the wild world of object storage and get any help you need. We&amp;#39;ve also set up Answer Overflow so that you can browse the Q&amp;amp;A from the web.
-        Today I&amp;#39;m going to discuss…`,
-    "source_title": "Xe Iaso&#39;s blog",
-    "source_link": "https://xeiaso.net/",
-    "date": "January 27, 2026"
   },
   
   {
