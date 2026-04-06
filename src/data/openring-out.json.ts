45efb0f254793604beae6cb89366b081fc5633c1
edmundmiller <20095261+edmundmiller@users.noreply.github.com>
2026-04-06 00:37:11 +0000

    Run Openring

--- src/data/openring-out.json.ts
+++ src/data/openring-out.json.ts
@@ -11,6 +11,15 @@ export const openringData: OpenringItem[] =
 [
   
   {
+    "title": "Claude Code won April Fools Day this year",
+    "link": "https://xeiaso.net/notes/2026/claude-code-wins-april-fools/",
+    "summary": `They gave people a heckin tamagochi, what&amp;#39;s not to like?`,
+    "source_title": "Xe Iaso&#39;s blog",
+    "source_link": "https://xeiaso.net/",
+    "date": "April 1, 2026"
+  },
+  
+  {
     "title": "tar: a slop-free alternative to rsync",
     "link": "https://drewdevault.com/2026/03/28/2026-03-28-rsync-without-rsync.html",
     "summary": `So apparently rsync is slop now. When I heard, I wanted to drop a quick
@@ -20,15 +29,6 @@ definitely accomodate th…`,
     "source_title": "Drew DeVault&#39;s blog",
     "source_link": "https://drewdevault.com",
     "date": "March 28, 2026"
-  },
-  
-  {
-    "title": "Homelab downtime update: The fight for DNS supremacy",
-    "link": "https://xeiaso.net/notes/2026/dns-fight/",
-    "summary": `Turns out everything DID NOT go offline somehow. Yay!`,
-    "source_title": "Xe Iaso&#39;s blog",
-    "source_link": "https://xeiaso.net/",
-    "date": "March 18, 2026"
   },
   
   {
