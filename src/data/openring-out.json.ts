56cb4bc3475d02d034b033bc1be3d37eff159c14
edmundmiller <20095261+edmundmiller@users.noreply.github.com>
2026-05-18 00:51:31 +0000

    Run Openring

--- src/data/openring-out.json.ts
+++ src/data/openring-out.json.ts
@@ -11,12 +11,22 @@ export const openringData: OpenringItem[] =
 [
   
   {
-    "title": "Maybe you shouldn&#39;t install new software for a bit",
-    "link": "https://xeiaso.net/blog/2026/abstain-from-install/",
-    "summary": `Oh boy yet more linux kernel vulns`,
+    "title": "Add an LLM policy for rust-lang/rust",
+    "link": "https://drewdevault.com/blog/LLM-policy-for-Rust/",
+    "summary": `
+            No comment on this PR may mention the following topics:Long-term social or economic impact of LLMsThe environmental impact of LLMsAnything to do with the copyright status of LLM outputMoral judgements about people who use LLMsWe have asked the…`,
+    "source_title": "Drew DeVault&#39;s blog",
+    "source_link": "https://drewdevault.com",
+    "date": "May 15, 2026"
+  },
+  
+  {
+    "title": "Amazonbot is finally respecting robots.txt",
+    "link": "https://xeiaso.net/notes/2026/amazonbot-respecting-robots-txt/",
+    "summary": `Thanks for giving me a viable business model Amazon!`,
     "source_title": "Xe Iaso&#39;s blog",
     "source_link": "https://xeiaso.net/",
-    "date": "May 7, 2026"
+    "date": "May 14, 2026"
   },
   
   {
@@ -25,16 +35,6 @@ export const openringData: OpenringItem[] =
     "summary": `My mother passed away suddenly last Saturday, 4/25. There aren’t enough words that can capture all the grief that I am feeling and all my memories of her. But I want to share how one small incident in our shared lives has enormously impacted how I pursue …`,
     "source_title": "News - Functional Genomics Laboratory",
     "source_link": "https://taehoonkim.org/news/",
-    "date": "May 1, 2026"
-  },
-  
-  {
-    "title": "I can&#39;t cancel GitHub Copilot",
-    "link": "https://drewdevault.com/blog/I-cant-cancel-Copilot/",
-    "summary": `
-            Back when Copilot first came out, I immediately disliked it. But I decided to give it a fair shake and tried to evaulate it in good faith. I wasn’t interested in paying for it, but they had a form for FOSS community members to apply for a free…`,
-    "source_title": "Drew DeVault&#39;s blog",
-    "source_link": "https://drewdevault.com",
     "date": "May 1, 2026"
   },
   
